abb463a49596f6672eb98fedb04e96a843a21358
Qwal <qwalski@gmail.com>
2016-10-03 11:03:17 +0200

    Initial setup with angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { FramappAngular2Page } from './app.po';
+
+describe('framapp-angular2 App', function() {
+  let page: FramappAngular2Page;
+
+  beforeEach(() => {
+    page = new FramappAngular2Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
