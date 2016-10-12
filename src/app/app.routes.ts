5df18d267b671cc47d18991721d63be000960ffc
arichter <arichter@pgs-soft.com>
2016-10-12 16:54:41 +0200

    cleanup in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import {Routes, RouterModule} from '@angular/router';
 import {Home} from './views/home';
-// import {WorkBoard} from './components/work-board';
-// import {About} from './about';
 import {NoContent} from './views/no-content';
-
-// import {DataResolver} from './app.resolver';
-
 
 export const AppRoutes: Routes = [
   {path: '', component: Home},
-  // {path: 'home', component: Home},
+  {path: 'home', component: Home},
   {path: '**', component: NoContent},
 ];
