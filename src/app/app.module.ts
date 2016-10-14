363173911a5bf5c0a5710cf660e3c12ba827de92
skala1988 <robert.kubisiak8@gmail.com>
2016-10-14 10:40:15 +0200

    rxjs map/catch imports -> global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,13 @@ import {AppRoutes} from './app.routes';
 import { KeysPipe } from './pipes/keys.pipe';
 import {HttpService} from "./services/http.service";
 
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
+
 
 @NgModule({
   declarations: [
