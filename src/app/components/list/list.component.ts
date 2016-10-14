6bdf66b1a2d0ce6cc94625f218cf50889aa021d1
skala1988 <robert.kubisiak8@gmail.com>
2016-10-14 15:16:32 +0200

    add new files to vcs

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Category} from "../../services/categories.service";
+
+@Component({
+  selector: 'app-list',
+  templateUrl: 'list.component.html',
+  styleUrls: ['list.component.scss'],
+  inputs: ['list:list','header:header']
+})
+export class ListComponent implements OnInit {
+  private list : Category = {};
+  private header : string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
