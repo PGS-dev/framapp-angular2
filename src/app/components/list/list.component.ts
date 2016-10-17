20cec19c98355ea8e677c0f5b70a7d960e26ecd7
skala1988 <robert.kubisiak8@gmail.com>
2016-10-17 11:08:20 +0200

    list mixins global scss variables global scss

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -5,11 +5,12 @@ import {Category} from "../../services/categories.service";
   selector: 'app-list',
   templateUrl: 'list.component.html',
   styleUrls: ['list.component.scss'],
-  inputs: ['list:list','header:header']
+  inputs: ['list:list','header:header','icoClasses:icoClasses']
 })
 export class ListComponent implements OnInit {
   private list : Category = {};
   private header : string = '';
+  private icoClasses : string = '';
 
   constructor() { }
 
