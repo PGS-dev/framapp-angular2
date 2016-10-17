9ed5ade2ab684a1a982520a955dde4db59c50cd7
skala1988 <robert.kubisiak8@gmail.com>
2016-10-17 13:07:14 +0200

    NavService (BehaviorSubject for events between components handling)

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -3,15 +3,19 @@
  */
 
 import { Component, OnInit } from '@angular/core';
-import { CategoriesService, Category  } from '../../services/categories.service';
+import { CategoriesService, Category } from '../../services/categories.service';
+import { NavService } from "../../services/nav.service";
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'cats',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss'],
-  providers:  [ CategoriesService ]
+  providers:  [ CategoriesService, NavService ]
 })
 export class Categories implements OnInit {
+  private subscription:Subscription;
+  private isMenuVisible : boolean = false;
   public categoryList : Category = {};
   private isAdmin : boolean = true;
   public adminMenu : Category = {
@@ -27,10 +31,13 @@ export class Categories implements OnInit {
     }
   };
 
-  constructor(private categoriesService: CategoriesService){};
+  constructor(private categoriesService: CategoriesService, private NavService:NavService){};
 
   ngOnInit(){
     this.getCategories();
+    this.subscription = this.NavService._isVisible$.subscribe(
+      isVisible => this.isMenuVisible = isVisible
+    );
   }
   getCategories(){
     this.categoriesService.getCategories()
