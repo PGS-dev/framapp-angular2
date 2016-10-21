9198709e897daee00bf8a34d80396374d231a71d
arichter <arichter@pgs-soft.com>
2016-10-21 17:01:33 +0200

    Firebase authentication support in service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,49 +1,50 @@
 import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs/Subject';
 
-import {AngularFire} from 'angularfire2';
+import {AngularFire, FirebaseAuthState} from 'angularfire2';
 
 @Injectable()
 export class AuthService {
-  private adminLogin: string = 'admin';
-  private adminPass: string = 'pass';
-
   private authState: AuthState = {
     isAuthenticated: false,
-    authUser: ''
+    authUser: '',
+    firebaseState: null
   };
+
   private _authSource = new Subject<AuthState>();
-  // private firebase = new AngularFire();
+
   constructor(private firebase: AngularFire) {
+    this.firebase.auth.subscribe(auth => this.setAuthenticated(auth));
   }
 
   get authentication$() {
     return this._authSource.asObservable();
   }
 
-  private setAuthenticated(auth: boolean, user?: string) {
+  private setAuthenticated(auth: FirebaseAuthState) {
+    console.log('Authenticated:', auth !== null);
+    const username = auth ? auth.auth.email : '';
+
     this.authState = {
-      isAuthenticated: auth,
-      authUser: user || ''
+      isAuthenticated: auth !== null,
+      authUser: username,
+      firebaseState: auth || null
     };
     this._authSource.next(this.authState);
   }
 
   login(user, pass) {
-    console.log('logging in...');
     this.firebase.auth.login({email: user, password: pass});
-    if (user === this.adminLogin && pass === this.adminPass) {
-      this.setAuthenticated(true, user);
-    }
   }
 
   logout() {
-    this.setAuthenticated(false);
+    this.firebase.auth.logout();
   }
 
 }
 
 export type AuthState = {
   isAuthenticated: boolean,
-  authUser: string
+  authUser: string,
+  firebaseState: FirebaseAuthState
 }
