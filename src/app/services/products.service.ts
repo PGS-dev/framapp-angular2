7c4306add493a78dd698b1dc734fc65572b5ca2d
Wojciech Grabowski (PGS Software) <wgrabowski@pgs-soft.com>
2016-11-23 18:46:08 +0100

    Update products service methods

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { DataService } from './data.service';
 import { Product } from "../interfaces/product.interface";
+import { Category } from "../interfaces/categories.interface";
 
 @Injectable()
 export class ProductsService {
 
-  constructor(private dataService: DataService ) {
+  constructor(private dataService: DataService) {
     this.getProducts()
   }
 
-  getProducts(){
-    return this.dataService.getData('products.json').map(
+  getProducts() {
+    return this.dataService.getDataAsArray('products.json').map(
       response=>response as Product[]
     );
+  }
+
+  getProductsByCategory(category: Category) {
+    return this.getProducts().filter(product=>product.category === category);
   }
 
 }
