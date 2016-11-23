b97e97150a77ddd39b68d88469580f1f174c3b66
Wojciech Grabowski (PGS Software) <wgrabowski@pgs-soft.com>
2016-11-23 14:49:16 +0100

    #3 add Category Page to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 // import { Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { ProductPageComponent } from './components/admin/product-page/product-page.component';
-
+import { ProductPageComponent } from './components/product-page/product-page.component';
+import { CategoryPageComponent } from './components/category-page/category-page.component';
 
 export const AppRoutes = [
   { path: '', component: HomeComponent },
-  { path: 'categories', component: HomeComponent },
-  { path: 'categories/:id', component: HomeComponent },
+  { path: 'categories', component: CategoryPageComponent },
+  { path: 'categories/:id', component: CategoryPageComponent },
   { path: 'products', component: ProductPageComponent},
   { path: 'products/:id', component: ProductPageComponent },
   { path: '**', component: HomeComponent }
