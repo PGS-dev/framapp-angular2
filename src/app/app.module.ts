bb1d4319e0b4d4ea0a0ff891f6cf6ce39b621ca0
Mateusz Trześniowski <mateusz.trzesniowski@gmail.com>
2017-01-11 16:06:17 +0100

    Project structure, sidebar, header, home component

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, PreloadAllModules } from "@angular/router";
+
+import { ROUTES } from './app.routes';
+import { SharedModule } from './shared/shared.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NoContentComponent } from './no-content/no-content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NoContentComponent
+  ],
+  imports: [
+    SharedModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
