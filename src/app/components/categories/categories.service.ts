8938ee3d9f696bb37b16adc27eb3ac91b7aff2f7
Platonn <platonn@platonn.com>
2017-03-13 15:05:40 +0100

    Categories fetching: changed Promise to RxJS

--- src/app/components/categories/categories.service.ts
+++ src/app/components/categories/categories.service.ts
@@ -1,21 +1,17 @@
 import {Injectable}    from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-
 import {Category} from './category';
 import {Config} from '../../../config';
 import {ErrorService} from '../../shared/ErrorService';
-
-
+import {Observable} from 'rxjs';
 @Injectable()
 export class CategoriesService {
   constructor(private http: Http, private errorService: ErrorService) {
   }
-
-  getCategories(): Promise<Category[]> {
+  getCategories(): Observable<Category[]> {
     return this.http.get(Config.getResourceUrl('categories'))
-      .toPromise()
-      .then(response => response.json() as Category[])
+      .map(response => response.json() as Category[])
       .catch(this.errorService.handle);
   }
 }
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,9 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-
 import {CategoriesService} from '../categories/categories.service';
 import {Category} from '../categories/category';
 import {ErrorService} from '../../shared/ErrorService';
-
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -11,25 +9,25 @@ import {ErrorService} from '../../shared/ErrorService';
   providers: [CategoriesService, ErrorService]
 })
 export class NavigationComponent implements OnInit {
-
   private categories: Category[];
-
   constructor(private categoryService: CategoriesService) {
   }
-
   getCategories(): void {
-    this.categoryService.getCategories()
-      .then(response => {
+    this.categoryService.getCategories().subscribe(
+      response => {
         this.categories = Object.keys(response).map((key) =>
           new Category(
             key,
             response[key].title,
             response[key].description
           )
-        );
-      });
+        )
+      },
+      error => {
+        console.error(error)
+      }
+    );
   }
-
   ngOnInit(): void {
     this.getCategories();
   }
